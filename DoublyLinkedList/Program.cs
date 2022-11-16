540667efaf33def71fb7e7271ecf783fac3e50ce
Yogaprstyae <m.yoga.ft21@mail.umy.ac.id>
2022-11-16 18:58:26 +0700

    Add int, string, and node

--- DoublyLinkedList/Program.cs
+++ DoublyLinkedList/Program.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace DoublyLinkedList
 {
-    internal class Program
+    class Node
     {
+        /* Node class represents the node of doubly linked list.
+         * It consists of the information part and links to
+         * its succeeding and preceeding nodes
+         * in terms of next and previous nodes. */
+        public int rollNumber;
+        public string name;
+        public Node next; /*points to the succeeding node*/
+        public Node prev; /*points to the preceeding node*/
         static void Main(string[] args)
         {
         }
